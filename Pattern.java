e6fa7856bf60c879045a396dedd69754b2d6a080
Sai Teja Chowderpally <chowderpally.saiteja@gmail.com>
2018-12-04 00:23:29 +0530

    Wrapping Up

--- Pattern.java
+++ Pattern.java
@@ -6,11 +6,14 @@ public class Pattern
     {
         System.out.println("****Welcome to Triangle/Diamond Printer****");
         System.out.println("Which Pattern do yout want to print?");
+        System.out.println();
         System.out.println("1) 54321    2)     1    3) 12345    4)   1");
         System.out.println("   5432           12        2345        123");
         System.out.println("   543           123         345       12345");
         System.out.println("   54           1234          45        123");
         System.out.println("   5           12345           5         1 ");
+        System.out.println();
+        System.out.print("Choose the Pattern(1-4 & 5 to quit):");
     }
 
     public void printPattern1(int rows)
@@ -91,94 +94,128 @@ public class Pattern
 			k = k + 1;
 		}
     }
+
+    public void startExecution()
+    {
+        Pattern pt1 = new Pattern();
+        
+        boolean askAgain = true;
+        while(askAgain)
+        {
+            pt1.printWelcomeMsg();
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+
+            switch(choice)
+            {
+                case 1:
+                {
+                    Scanner sc1 = new Scanner(System.in);
+
+                    boolean again = true;
+                    while(again)
+                    {
+                        System.out.print("How many rows would you like to print (More than 1 please):");
+                        int input = sc1.nextInt();
+
+                        if (input > 1 && input < 10)
+                        {
+                            pt1.printPattern1(input);
+                            again = false;
+                        }
+                        else
+                        {
+                            System.out.println("Illegal entry. Try again!");
+                        }
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    Scanner sc1 = new Scanner(System.in);
+
+                    boolean again = true;
+                    while(again)
+                    {
+                        System.out.print("How many rows would you like to print (More than 1 please):");
+                        int input = sc1.nextInt();
+
+                        if (input > 1 && input < 10)
+                        {
+                            pt1.printPattern2(input);
+                            again = false;
+                        }
+                        else
+                        {
+                            System.out.println("Illegal entry. Try again!");
+                        }
+                    }
+                    break;
+                }
+                case 3:
+                {
+                    Scanner sc1 = new Scanner(System.in);
+
+                    boolean again = true;
+                    while(again)
+                    {
+                        System.out.print("How many rows would you like to print (More than 1 please):");
+                        int input = sc1.nextInt();
+
+                        if (input > 1 && input < 10)
+                        {
+                            pt1.printPattern3(input);
+                            again = false;
+                        }
+                        else
+                        {
+                            System.out.println("Illegal entry. Try again!");
+                        }
+                    }
+                    break;
+                }
+                case 4:
+                {
+                    Scanner sc1 = new Scanner(System.in);
+
+                    boolean again = true;
+                    while(again)
+                    {
+                        System.out.print("How many rows would you like to print (More than 1 please):");
+                        int input = sc1.nextInt();
+
+                        if (input > 1 && input < 10)
+                        {
+                            pt1.printPattern4(input);
+                            again = false;
+                        }
+                        else
+                        {
+                            System.out.println("Illegal entry. Try again!");
+                        }
+                    }
+                    break;
+                }
+                case 5:
+                {
+                    System.out.println("Thank you! Hope you enjoyed...");
+                    askAgain = false;
+                    break;
+                }
+                default:
+                {
+                    System.out.println("Illegal entry. Try again!");
+                    break;
+                }
+            }
+
+        }
+
+    }
     
     public static void main(String args[])
     {
         Pattern pt = new Pattern();
-        pt.printWelcomeMsg();
-        
-        System.out.print("Choose the Pattern(1-4 & 5 to quit):");
-        
-        Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        System.out.println(choice);
-
-        switch(choice)
-        {
-            case 1:
-            {
-                Scanner sc1 = new Scanner(System.in);
-                System.out.print("How many rows would you like to print (More than 1 please):");
-                int input = sc1.nextInt();
-                if (input > 1 && input <= 10)
-                {
-                    pt.printPattern1(input);
-                    break;
-                }
-                else
-                {
-                    System.out.println("Illegal entry. Try again!");
-                    break;
-                }
-            }
-            case 2:
-            {
-                Scanner sc1 = new Scanner(System.in);
-                System.out.print("How many rows would you like to print (More than 1 please):");
-                int input = sc1.nextInt();
-                if (input > 1 && input <= 10)
-                {
-                    pt.printPattern2(input);
-                    break;
-                }
-                else
-                {
-                    System.out.println("Illegal entry. Try again!");
-                    break;
-                }
-            }
-            case 3:
-            {
-                Scanner sc1 = new Scanner(System.in);
-                System.out.print("How many rows would you like to print (More than 1 please):");
-                int input = sc1.nextInt();
-                if (input > 1 && input <= 10)
-                {
-                    pt.printPattern3(input);
-                    break;
-                }
-                else
-                {
-                    System.out.println("Illegal entry. Try again!");
-                    break;
-                }
-            }
-            case 4:
-            {
-                Scanner sc1 = new Scanner(System.in);
-                System.out.print("How many rows would you like to print (More than 1 please):");
-                int input = sc1.nextInt();
-                if (input > 1 && input <= 10)
-                {
-                    pt.printPattern4(input);
-                    break;
-                }
-                else
-                {
-                    System.out.println("Illegal entry. Try again!");
-                    break;
-                }
-            }
-            case 5:
-            {
-                System.out.println("Thank you! Hope you enjoyed...");
-                break;
-            }
-            default:
-            {
-                System.out.println("Illegal entry. Try again!");
-                break;
-            }
-        }
+        pt.startExecution();
     }
 }
